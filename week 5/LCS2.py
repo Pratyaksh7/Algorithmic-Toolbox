b7a33d80bbaacefecb3a4075ffc7cc6350d11ccb
Pratyaksh7 <pratyakshgupta7@gmail.com>
2020-08-18 12:40:04 +0530

    optimized  Recursive Solution LCS of 2 sequence

--- week 5/LCS2.py
+++ week 5/LCS2.py
@@ -1,14 +1,23 @@
+
 def lcs(l1,l2,n,m):
     if n == 0 or m == 0:
         return 0
+    if t[n][m] != -1:
+        return t[n][m]
+
     if l1[n-1] == l2[m-1] :
-        return (1+ lcs(l1,l2,n-1,m-1))
+        # storing the recursive value in the t matrix for future values of recursive calls
+        t[n][m]= (1+ lcs(l1,l2,n-1,m-1))
+        return t[n][m]
     else:
-        return max(lcs(l1,l2,n,m-1),lcs(l1,l2,n-1,m))
+        t[n][m] = max(lcs(l1,l2,n,m-1),lcs(l1,l2,n-1,m))
+        return t[n][m]
 
 n = int(input())
 l1 = list(map(int, input().split()))
 m = int(input())
 l2 = list(map(int, input().split()))
 
+#memoization : initializing a matrix with -1 then updating its value on recursive call
+t = [[-1 for j in range(m+1)] for i in range(n+1) ]      # m and n should be incremented my 1 to access index m and n i.e., t[n][m]
 print(lcs(l1,l2,n,m))
